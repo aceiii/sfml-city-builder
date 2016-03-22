9a16954501ce4b9967438556f1c14b648b548bea
Borin Ouch <borin.ouch@gmail.com>
2016-03-22 11:43:44 -0700

    Update main to use game state start

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,14 @@
 
 
 #include "game.h"
+#include "game_state_start.h"
 
 
 int main(int argc, char* argv[]) {
 
     Game game;
+
+    game.pushState(new GameStateStart(&game));
     game.gameLoop();
 
     return 0;
