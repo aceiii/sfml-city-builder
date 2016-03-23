0b3505726e6a58409f8dd8ca99b4abbfd3ef5e07
Borin Ouch <borin.ouch@gmail.com>
2016-03-22 23:42:24 -0700

    Call loadGame() on space key

--- src/game_state_start.cpp
+++ src/game_state_start.cpp
@@ -41,6 +41,9 @@ void GameStateStart::handleInput() {
                 if (event.key.code == sf::Keyboard::Escape) {
                     game->window.close();
                 }
+                else if (event.key.code == sf::Keyboard::Space) {
+                    loadGame();
+                }
                 break;
             }
             default:
