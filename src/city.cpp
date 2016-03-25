49b56163bdc335fca17ebc01f97917055d1b7d0b
Borin Ouch <borin.ouch@gmail.com>
2016-03-24 20:49:06 -0700

    Implement city methods

--- src/city.cpp
+++ src/city.cpp
@@ -2,7 +2,11 @@
 // Created by Borin Ouch on 2016-03-24.
 //
 
+
+#include <numeric>
+
 #include "city.h"
+
 
 City::City() {
     birthRate = 0.00055;
@@ -40,15 +44,32 @@ void City::update(float dt) {
 }
 
 void City::bulldoze(const Tile &tile) {
-
+    for (int pos = 0; pos < map.width * map.height; pos += 1) {
+        if (map.selected[pos] == 1) {
+            if (map.tiles[pos].tileType == TileType::RESIDENTIAL) {
+                populationPool += map.tiles[pos].population;
+            } else if (map.tiles[pos].tileType == TileType::COMMERCIAL) {
+                employmentPool += map.tiles[pos].population;
+            } else if (map.tiles[pos].tileType == TileType::INDUSTRIAL) {
+                employmentPool += map.tiles[pos].population;
+            }
+            map.tiles[pos] = tile;
+        }
+    }
 }
 
 void City::shuffleTiles() {
+    while (shuffledTiles.size() < map.tiles.size()) {
+        shuffledTiles.push_back(0);
+    }
 
+    std::iota(shuffledTiles.begin(), shuffledTiles.end(), 1);
+    std::random_shuffle(shuffledTiles.begin(), shuffledTiles.end());
 }
 
 void City::tileChanged() {
-
+    map.updateDirection(TileType::ROAD);
+    map.findConnectedRegions({ TileType::ROAD, TileType::RESIDENTIAL, TileType::COMMERCIAL, TileType::INDUSTRIAL }, 0);
 }
 
 double City::getHomeless() {
@@ -60,5 +81,31 @@ double City::getUnemployed() {
 }
 
 double City::distributionPool(double &pool, Tile &tile, double rate) {
-    return 0;
+    const static int moveRate = 4;
+
+    double maxPop = tile.maxPopPerLevel * (tile.tileVariant + 1);
+
+    if (pool > 0) {
+        double moving = maxPop - tile.population;
+
+        if (moving > moveRate) {
+            moving = moveRate;
+        }
+
+        if (pool - moving < 0) {
+            moving = pool;
+        }
+
+        pool -= moving;
+        tile.population += moving;
+    }
+
+    tile.population += tile.population * rate;
+
+    if (tile.population > maxPop) {
+        pool += tile.population - maxPop;
+        tile.population = maxPop;
+    }
+
+    return tile.population;
 }
