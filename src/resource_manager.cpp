21e46d0b0992d9b489c1b1ffecb1c1083c43c3af
Borin Ouch <borin.ouch@gmail.com>
2016-03-28 14:20:38 -0700

    Add resource manager

---
+++ src/resource_manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Borin Ouch on 2016-03-27.
+//
+
+#include "resource_manager.h"
---
+++ src/resource_manager.h
@@ -0,0 +1,14 @@
+//
+// Created by Borin Ouch on 2016-03-27.
+//
+
+#ifndef SFML_TEST_RESOURCE_MANAGER_H
+#define SFML_TEST_RESOURCE_MANAGER_H
+
+
+class resource_manager {
+
+};
+
+
+#endif //SFML_TEST_RESOURCE_MANAGER_H
